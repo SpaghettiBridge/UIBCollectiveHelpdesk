4c3e3c6e52dc7e50dacdbcdb79bb85b47df17d7a
UI Bakery <chris.bates@quay-tech.co.uk>
2024-06-18 12:58:40 +0000

    fixed ordering of tickets

--- pages/tickets/actions/Get Data/get_All_tickets/query-preparedQuery.sql
+++ pages/tickets/actions/Get Data/get_All_tickets/query-preparedQuery.sql
@@ -1,2 +1,9 @@
 select  * from vw_all_tickets
 where  vw_all_tickets.service_id in(?) 
+ORDER BY 
+    CASE vw_all_tickets.priority
+        WHEN 'High' THEN 1
+        WHEN 'Medium' THEN 2
+        WHEN 'Low' THEN 3
+        ELSE 4
+    END;
--- pages/tickets/actions/Get Data/get_My_tickets/query-query.sql
+++ pages/tickets/actions/Get Data/get_My_tickets/query-query.sql
@@ -1,2 +1,9 @@
 select  * from vw_all_tickets
 where  vw_all_tickets.fk_agent_id = {{state.AccountData.account_id}}
+ORDER BY 
+    CASE vw_all_tickets.priority
+        WHEN 'High' THEN 1
+        WHEN 'Medium' THEN 2
+        WHEN 'Low' THEN 3
+        ELSE 4
+    END;
