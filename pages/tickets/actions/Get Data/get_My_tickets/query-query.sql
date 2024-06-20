da525bd5fdde33c53cb44110759c1620b4880d9d
UI Bakery <chris.bates@quay-tech.co.uk>
2024-06-20 09:59:08 +0000

    added CC field

--- pages/tickets/actions/Get Data/get_My_tickets/query-query.sql
+++ pages/tickets/actions/Get Data/get_My_tickets/query-query.sql
@@ -1,6 +1,7 @@
 select  * from vw_all_tickets
 where  vw_all_tickets.fk_agent_id = {{state.AccountData.account_id}}
 or vw_all_tickets.fk_creator_id = {{state.AccountData.account_id}}
+
 ORDER BY 
     CASE vw_all_tickets.priority
         WHEN 'High' THEN 1
