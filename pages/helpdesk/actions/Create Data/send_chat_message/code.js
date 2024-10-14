3f6520d2203bc2ba9deba7167f7983e8184d83a0
UI Bakery <chris.bates@spbridge.co.uk>
2024-10-14 16:46:15 +0000

    lots of fixes from email list of last demo

--- pages/helpdesk/actions/Create Data/send_chat_message/code.js
+++ pages/helpdesk/actions/Create Data/send_chat_message/code.js
@@ -9,8 +9,8 @@
 {{ui.txt_ext_message6.reset()}}
 {{ui.msg_attach6.reset()}}
 
-const modalid = steps.params.data.called_by; 
-//looks broken but works to close the modal that called this action
-ui[modalid].close()
+// const modalid = steps.params.data.called_by; 
+// //looks broken but works to close the modal that called this action
+// ui[modalid].close()
 
 
